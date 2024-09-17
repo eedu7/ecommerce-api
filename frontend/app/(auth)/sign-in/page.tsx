5cf0fc58a1f5d9de4d9f0e728e61f733109a2993
eedu7 <mueedahmad067@gmail.com>
2024-09-17 19:14:19 +0500

    FEAT: added the sign up form

---
+++ frontend/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Button} from "@/components/ui/button";
+
+const Page = () => {
+    return (
+        <>
+            <Button size="lg">Sign In</Button>
+        </>
+    )
+}
+export default Page

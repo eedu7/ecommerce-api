79595b722d1d9f6b4e42916758d0c238fba1eb53
eedu7 <mueedahmad067@gmail.com>
2024-09-19 21:44:05 +0500

    Initial Project Setup

--- frontend/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-      <main className="max-w-7xl mx-auto bg-zinc-900 h-[100vh] flex justify-center items-center ">
-          <div className="size-44 border border-gray-200 relative bottom-44 ">
-              <h1 className="text-2xl text-white font-semibold">Home Page</h1>
-          </div>
-      </main>
-  );
-}
---
+++ frontend/src/features/auth/components/auth-screen.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export const AuthScreen = () => {
+    return (
+      <div className="h-full flex items-center justify-center">
+          <div className="md:h-auto md:w-[420px]">
+              Auth Screen
+          </div>
+      </div>
+    );
+}

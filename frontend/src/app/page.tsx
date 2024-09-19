d59d75f34cadce8958394fe22ecf2b2121df0f2c
eedu7 <mueedahmad067@gmail.com>
2024-09-19 22:20:13 +0500

    Added the auth components i.e Sign in and sign up card.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,6 +2,6 @@ import {AuthScreen} from "@/features/auth/components/auth-screen";
 
 export default function Home() {
   return (
-  <AuthScreen />
+      <AuthScreen />
   );
 }
--- frontend/src/features/auth/components/auth-screen.tsx
+++ frontend/src/features/auth/components/auth-screen.tsx
@@ -1,11 +1,17 @@
 "use client";
 
+import {useState} from "react";
+import {SignInFlow} from "@/features/auth/types";
+import {SignInCard} from "@/features/auth/components/sign-in-card";
+import {SignUpCard} from "@/features/auth/components/sign-up-card";
+
 export const AuthScreen = () => {
-    return (
-      <div className="h-full flex items-center justify-center">
-          <div className="md:h-auto md:w-[420px]">
-              Auth Screen
-          </div>
-      </div>
-    );
-}
+    const [state, setState] = useState<SignInFlow>("signIn");
+    return <div className="h-full flex items-center justify-center">
+        <div className="md:h-auto md:w-[420px]">
+            {state === "signIn" ? <SignInCard setState={setState} /> : <SignUpCard setState={setState} />}
+        </div>
+    </div>
+};
+
+

f3b879ee2a93928cff40d98de615fe1a5ba0f1d2
eedu7 <mueedahmad067@gmail.com>
2024-10-03 12:04:06 +0500

    MINOR: code refactor and code organizing

--- frontend/src/features/auth/sign-up/api.ts
+++ frontend/src/features/auth/sign-up/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-export async function registerUser (data: UserRegisterData): Promise<UserRegisterResponse> {
+export async function registerUser(data: UserRegisterData) {
     const response = await axios.post(`${process.env.NEXT_PUBLIC_BASE_API_URL}/users/`, data);
     return response.data;
 }
--- frontend/src/features/auth/sign-up/types.ts
+++ frontend/src/features/auth/sign-up/types.ts
@@ -1,7 +1,3 @@
 type UserRegisterData = {
     name: string, email: string, password: string,
 }
-
-type UserRegisterResponse = {
-    id: number, email: string, name: string, is_active: boolean,
-}

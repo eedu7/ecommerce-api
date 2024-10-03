d917f940abb4f44cec852c82a116084a4fc30c09
eedu7 <mueedahmad067@gmail.com>
2024-10-03 17:26:43 +0500

    MINOR: modified the sign-in and sign-up page

--- frontend/src/app/(auth)/sign-up/page.tsx
+++ frontend/src/app/(auth)/sign-up/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import {Card, CardContent, CardDescription, CardHeader, CardTitle} from "@/components/ui/card";
+import {Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} from "@/components/ui/card";
 import Image from "next/image";
 import LogoIpsumIcon from "@/assets";
 import {Separator} from "@/components/ui/separator";
 import SignUpForm from "@/app/(auth)/sign-up/sign-up-form";
+import Link from "next/link";
 
 const SignUpPage = () => {
     return <Card className="w-[400px]">
@@ -18,6 +19,14 @@ const SignUpPage = () => {
         <CardContent>
             <SignUpForm/>
         </CardContent>
+        <CardFooter>
+            <Link href="/sign-in" className="w-full">
+                <p className="text-sm text-right w-full text-muted-foreground">
+                    Already have an account? <span
+                    className="text-sky-300 cursor-pointer hover:underline">Login here!</span>
+                </p>
+            </Link>
+        </CardFooter>
     </Card>
 }
 export default SignUpPage

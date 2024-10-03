d38e4eb96fb1e6685574cbba8c7c2625e4034a6a
eedu7 <mueedahmad067@gmail.com>
2024-10-03 18:09:21 +0500

    MINOR: added the rows actions i.e edit and delete

--- frontend/src/app/admin/dashboard/(data-modals)/payment/columns.tsx
+++ frontend/src/app/admin/dashboard/(data-modals)/payment/columns.tsx
@@ -1,6 +1,10 @@
 "use client"
 
 import {ColumnDef} from "@tanstack/react-table"
+import {Edit, MoreHorizontal, Trash} from "lucide-react"
+
+import {Button} from "@/components/ui/button"
+import {DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger,} from "@/components/ui/dropdown-menu"
 
 export type Payment = {
     id: string
@@ -35,4 +39,23 @@ export const columns: ColumnDef<Payment>[] = [{
         }).format(amount);
         return <div className="text-right font-medium">{formatted}</div>
     }
+}, {
+    id: "actions", cell: ({row}) => {
+        const payment = row.original
+
+        return (<div className="flex justify-end"><DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button variant="ghost" className="h-8 w-8 p-0">
+                    <span className="sr-only">Open menu</span>
+                    <MoreHorizontal className="h-4 w-4"/>
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+                <DropdownMenuItem className="space-x-2 flex items-center"><Edit className="size-4"/>
+                    <span>Edit</span></DropdownMenuItem>
+                <DropdownMenuItem className="space-x-2 flex items-center"><Trash className="size-4"/>
+                    <span>Delete</span></DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu></div>)
+    },
 },]

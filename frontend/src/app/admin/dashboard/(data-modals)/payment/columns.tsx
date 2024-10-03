0581545d9e98b5408daaafa2b6b149ee6ca6f2f4
eedu7 <mueedahmad067@gmail.com>
2024-10-03 18:16:32 +0500

    MINOR: added sorting to the email column in the payment data table

--- frontend/src/app/admin/dashboard/(data-modals)/payment/columns.tsx
+++ frontend/src/app/admin/dashboard/(data-modals)/payment/columns.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import {ColumnDef} from "@tanstack/react-table"
-import {Edit, MoreHorizontal, Trash} from "lucide-react"
+import {ArrowUpDown, Edit, MoreHorizontal, Trash} from "lucide-react"
 
 import {Button} from "@/components/ui/button"
 import {DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger,} from "@/components/ui/dropdown-menu"
@@ -30,7 +30,15 @@ export const columns: ColumnDef<Payment>[] = [{
 
     }
 }, {
-    accessorKey: "email", header: "Email",
+    accessorKey: "email", header: ({column}) => {
+        return (<Button
+                variant="ghost"
+                onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+            >
+                Email
+                <ArrowUpDown className="ml-2 h-4 w-4"/>
+            </Button>)
+    },
 }, {
     accessorKey: "amount", header: () => <div className="text-right">Amount</div>, cell: ({row}) => {
         const amount = parseFloat(row.getValue("amount"));
--- frontend/src/app/admin/dashboard/(data-modals)/payment/data-table.tsx
+++ frontend/src/app/admin/dashboard/(data-modals)/payment/data-table.tsx
@@ -1,9 +1,12 @@
 "use client"
 
-import {ColumnDef, flexRender, getCoreRowModel, getPaginationRowModel, useReactTable,} from "@tanstack/react-table"
+import {
+    ColumnDef, flexRender, getCoreRowModel, getPaginationRowModel, getSortedRowModel, SortingState, useReactTable,
+} from "@tanstack/react-table"
 
 import {Table, TableBody, TableCell, TableHead, TableHeader, TableRow,} from "@/components/ui/table"
 import {Button} from "@/components/ui/button";
+import React from "react";
 
 interface DataTableProps<TData, TValue> {
     columns: ColumnDef<TData, TValue>[]
@@ -13,8 +16,18 @@ interface DataTableProps<TData, TValue> {
 export function DataTable<TData, TValue>({
                                              columns, data,
                                          }: DataTableProps<TData, TValue>) {
+    const [sorting, setSorting] = React.useState<SortingState>([])
+
     const table = useReactTable({
-        data, columns, getCoreRowModel: getCoreRowModel(), getPaginationRowModel: getPaginationRowModel(),
+        data,
+        columns,
+        getCoreRowModel: getCoreRowModel(),
+        getPaginationRowModel: getPaginationRowModel(),
+        onSortingChange: setSorting,
+        getSortedRowModel: getSortedRowModel(),
+        state: {
+            sorting
+        }
     })
 
     return (<div>
@@ -23,26 +36,26 @@ export function DataTable<TData, TValue>({
                 <Table>
                     <TableHeader>
                         {table.getHeaderGroups().map((headerGroup) => (<TableRow key={headerGroup.id}>
-                                {headerGroup.headers.map((header) => {
-                                    return (<TableHead key={header.id}>
-                                            {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
-                                        </TableHead>)
-                                })}
-                            </TableRow>))}
+                            {headerGroup.headers.map((header) => {
+                                return (<TableHead key={header.id}>
+                                    {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
+                                </TableHead>)
+                            })}
+                        </TableRow>))}
                     </TableHeader>
                     <TableBody>
                         {table.getRowModel().rows?.length ? (table.getRowModel().rows.map((row) => (<TableRow
-                                    key={row.id}
-                                    data-state={row.getIsSelected() && "selected"}
-                                >
-                                    {row.getVisibleCells().map((cell) => (<TableCell key={cell.id}>
-                                            {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                                        </TableCell>))}
-                                </TableRow>))) : (<TableRow>
-                                <TableCell colSpan={columns.length} className="h-24 text-center">
-                                    No results.
-                                </TableCell>
-                            </TableRow>)}
+                            key={row.id}
+                            data-state={row.getIsSelected() && "selected"}
+                        >
+                            {row.getVisibleCells().map((cell) => (<TableCell key={cell.id}>
+                                {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                            </TableCell>))}
+                        </TableRow>))) : (<TableRow>
+                            <TableCell colSpan={columns.length} className="h-24 text-center">
+                                No results.
+                            </TableCell>
+                        </TableRow>)}
                     </TableBody>
                 </Table>
             </div>

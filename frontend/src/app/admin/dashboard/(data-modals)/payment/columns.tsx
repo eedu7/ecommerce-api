468cdbc04120872bf415d7c17178894f8a7e317e
eedu7 <mueedahmad067@gmail.com>
2024-10-03 17:55:54 +0500

    MINOR: added the text colors to different payment status

--- frontend/src/app/admin/dashboard/(data-modals)/payment/columns.tsx
+++ frontend/src/app/admin/dashboard/(data-modals)/payment/columns.tsx
@@ -10,9 +10,29 @@ export type Payment = {
 }
 
 export const columns: ColumnDef<Payment>[] = [{
-    accessorKey: "status", header: "Status",
+    accessorKey: "status", header: "Status", cell: ({row}) => {
+        const value: string = row.getValue("status");
+        const titleCaseValue: string = value.charAt(0).toUpperCase() + value.slice(1);
+        switch (value) {
+            case "pending":
+                return <div className="text-yellow-500">{titleCaseValue}</div>
+            case "processing":
+                return <div className="text-blue-500">{titleCaseValue}</div>
+            case "success":
+                return <div className="text-green-500">{titleCaseValue}</div>
+            case "failed":
+                return <div className="text-red-500">{titleCaseValue}</div>
+        }
+
+    }
 }, {
     accessorKey: "email", header: "Email",
 }, {
-    accessorKey: "amount", header: "Amount",
+    accessorKey: "amount", header: () => <div className="text-right">Amount</div>, cell: ({row}) => {
+        const amount = parseFloat(row.getValue("amount"));
+        const formatted = new Intl.NumberFormat("en-US", {
+            style: "currency", currency: "USD",
+        }).format(amount);
+        return <div className="text-right font-medium">{formatted}</div>
+    }
 },]
